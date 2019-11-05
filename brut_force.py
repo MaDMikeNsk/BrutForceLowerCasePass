8790f8453da0ffa6f82a48b7ecddc2ff763af194
MaDMikeNsk <mikemenshikov85@gmail.com>
2019-11-06 01:34:42 +0700

    small changes

--- brut_force.py
+++ brut_force.py
@@ -9,17 +9,18 @@ def tuple_to_string(tup):
     return res
 
 
-def brut(password_length, correct_password):
+def brut(password_length: int, correct_password: str) -> str:
     print(f'Подождите, идёт подбор пароля из {password_length} символов...')
     char_string = string.ascii_lowercase  # Пароль состоит из маленьких англ букв
-    combination = it.product(char_string, repeat=password_length)  # Размещения с повторениями (iterator)
+    combination = it.product(char_string, repeat=password_length)  # Размещения с повторениями (iterator) type: tuple
 
     i = 0
     for i in range(1, len(char_string) ** password_length + 1):
         temp_comb = next(combination)
         if tuple_to_string(temp_comb) == correct_password:
-            return print(f'Пароль "{tuple_to_string(temp_comb)}" найден за {i} итерации(й)')
-    return print(f'Пароль не найден, итераций {i}')
+            return f'Пароль "{tuple_to_string(temp_comb)}" найден за {i} итерации(й)'
+    return f'Пароль не найден, итераций {i}'
 
 
-brut(2, 'gh')
+a = brut(4, 'z1zz')
+print(a)
